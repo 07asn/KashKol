1b92440a32658367899eb538c89f55dda40ac46f
Hasan Mansour <124068265+07asn@users.noreply.github.com>
2025-01-16 01:28:57 +0300

    Add files via upload

---
+++ KashKol/navbar.js
@@ -0,0 +1,11 @@
+fetch('navbar.html')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch navbar.html');
+                }
+                return response.text();
+            })
+            .then(data => {
+                document.getElementById('navbar-placeholder').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading navbar:', error));
